6e25a6d490001bc4e6b71e65a52eb0db41e09cb9
Nataly-tk <natasha.totska@yahoo.com>
2022-02-26 18:14:22 +0200

    Added file getGoods.js

--- js/search.js
+++ js/search.js
@@ -1,10 +1,10 @@
 const search = function () {
   const input = document.querySelector(".search-block > input");
   const searchBtn = document.querySelector(".search-block > button");
-  
-    searchBtn.addEventListener('click', (event) => {
-        const inputValue = input.value;
-        console.log(inputValue);
-    });
+
+  searchBtn.addEventListener("click", () => {
+    const inputValue = input.value;
+    console.log(inputValue);
+  });
 };
 search();
