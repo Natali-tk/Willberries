8543857a4e68902e201e52be009d34e61132e792
Nataly-tk <natasha.totska@yahoo.com>
2022-02-27 01:39:57 +0200

    finished work

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,169 @@
 const cart = function () {
-    const cartBtn = document.querySelector('.button-cart');
-    const cart = document.getElementById('modal-cart');
-    const closeBtn = document.querySelector('.modal-close');
-    
-    cartBtn.addEventListener("click", () => {
-        cart.style.display = "flex";
+  const cartBtn = document.querySelector(".button-cart");
+  const cart = document.getElementById("modal-cart");
+  const closeBtn = document.querySelector(".modal-close");
+  const goodContainer = document.querySelector(".long-goods-list");
+  const cartTable = document.querySelector(".cart-table__goods");
+  const modalForm = document.querySelector(".modal-form");
+  const sumElement = cart.querySelector(".card-table__total");
+
+  sumElement.innerHTML = "";
+  const getFromStorage = (name) => {
+    return localStorage.getItem(name)
+      ? JSON.parse(localStorage.getItem(name))
+      : [];
+  };
+
+  const updateSum = () => {
+    const cart = getFromStorage("cart");
+
+    let sum = 0;
+    cart.forEach((item) => {
+      sum += Number(item.price) * Number(item.count);
     });
-    closeBtn.addEventListener("click", () => {
-        cart.style.display = "";
+
+    sumElement.textContent = String(sum);
+  };
+
+  const deleteCartItem = (id) => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const newCart = cart.filter((good) => {
+      return good.id !== id;
     });
-}
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    renderCartGoods(JSON.parse(localStorage.getItem("cart")));
+  };
+  const plusCartItem = (id) => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const newCart = cart.map((good) => {
+      if (good.id === id) {
+        good.count++;
+      }
+      return good;
+    });
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    renderCartGoods(JSON.parse(localStorage.getItem("cart")));
+  };
+  const minusCartItem = (id) => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const newCart = cart.map((good) => {
+      if (good.id === id) {
+        if (good.count > 0) good.count--;
+      }
+      return good;
+    });
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    renderCartGoods(JSON.parse(localStorage.getItem("cart")));
+  };
+
+  const addToCart = (id) => {
+    const goods = JSON.parse(localStorage.getItem("goods"));
+    const clickedGood = goods.find((good) => good.id === id);
+    const cart = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+    if (cart.some((good) => good.id === clickedGood.id)) {
+      cart.map((good) => {
+        if (good.id === clickedGood.id) {
+          good.count++;
+        }
+        return good;
+      });
+    } else {
+      clickedGood.count = 1;
+      cart.push(clickedGood);
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+  };
+
+  const renderCartGoods = (goods) => {
+    cartTable.innerHTML = "";
+    goods.forEach((good) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+       <tr>
+			<td>${good.name}</td>
+			<td>${good.price}$</td>
+			<td><button class="cart-btn-minus"">-</button></td>
+			<td>${good.count}</td>
+			<td><button class=" cart-btn-plus"">+</button></td>
+			<td>${+good.price * +good.count}$</td>
+			<td><button class="cart-btn-delete"">x</button></td>
+		</tr>
+        `;
+      cartTable.append(tr);
+      tr.addEventListener("click", (e) => {
+        if (e.target.classList.contains("cart-btn-minus")) {
+          minusCartItem(good.id);
+        } else if (e.target.classList.contains("cart-btn-plus")) {
+          plusCartItem(good.id);
+        } else if (e.target.classList.contains("cart-btn-delete")) {
+          deleteCartItem(good.id);
+        }
+      });
+      updateSum();
+      sumElement.innerHTML = "";
+    });
+  };
+
+  closeBtn.addEventListener("click", () => {
+    cart.style.display = "";
+  });
+  cart.addEventListener("click", (e) => {
+    if (!e.target.closest(".modal") && e.target.classList.contains("overlay")) {
+      cart.style.display = "";
+    }
+  });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      cart.style.display = "";
+    }
+  });
+
+  cartBtn.addEventListener("click", () => {
+    const cartArray = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+    renderCartGoods(cartArray);
+    cart.style.display = "flex";
+  });
+
+  if (goodContainer) {
+    goodContainer.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (e.target.closest(".add-to-cart")) {
+        const buttonToCart = e.target.closest(".add-to-cart");
+        const goodId = buttonToCart.dataset.id;
+        addToCart(goodId);
+      }
+    });
+  }
+  const sendForm = () => {
+    let nameInput = document.getElementsByName("nameCustomer")[0];
+    let phoneInput = document.getElementsByName("phoneCustomer")[0];
+    let name = nameInput.value;
+    let phone = phoneInput.value;
+    const cartArray = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        cart: cartArray,
+        name: name,
+        phone: phone,
+      }),
+    }).then(() => {
+      cart.style.display = "";
+      localStorage.removeItem("cart");
+      cartTable.innerHTML = "";
+    });
+  };
+  modalForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendForm();
+  });
+};
 cart();
